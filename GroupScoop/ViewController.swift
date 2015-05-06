0298acee18a6934df8e40709e4d6b1f933e91d39
coryswynn <wynn.cory@gmail.com>
2015-05-05 23:35:32 -0500

    Initial Commit

---
+++ GroupScoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroupScoop
+//
+//  Created by Cory Wynn on 5/5/15.
+//  Copyright (c) 2015 Wynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
