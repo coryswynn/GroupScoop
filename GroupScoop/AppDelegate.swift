6d14b52a4215e62a82356a4646a207afed6104e6
coryswynn <wynn.cory@gmail.com>
2015-05-05 23:59:22 -0500

    Added Frameworks
    Added Parse SDK along with SwiftPrompt

--- GroupScoop/AppDelegate.swift
+++ GroupScoop/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Parse
+import Bolts
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("HyMF6eQRTYLmMZzlcmOVxQrwe3qhhDjW02jRgDb0",
+            clientKey: "TWwYz2Ywcltlkq24z6tDm01DuX333IlmCNV3xZ4P")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
