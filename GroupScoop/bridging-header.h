25e04ee1a6e5dedb29d843cce0e5d52dc848ac80
coryswynn <wynn.cory@gmail.com>
2015-05-28 02:29:07 -0700

    Integrated ProgressHUD

---
+++ GroupScoop/bridging-header.h
@@ -0,0 +1,14 @@
+//
+//  bridging-header.h
+//  GroupScoop
+//
+//  Created by Cory Wynn on 5/28/15.
+//  Copyright (c) 2015 Wynn. All rights reserved.
+//
+
+#ifndef GroupScoop_bridging_header_h
+#define GroupScoop_bridging_header_h
+
+#import "ProgressHUD.h"
+
+#endif
